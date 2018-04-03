b45c7d8cf3ef586bd06360f8df5b02468f3c2124
clkong <36968245+clkong@users.noreply.github.com>
2018-04-03 13:58:41 -0400

    Update FunwithLoops.java

--- src/funwithloops/FunwithLoops.java
+++ src/funwithloops/FunwithLoops.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sean Luo 29/03/2018
+ * FunwithLoop.java
+ * This program calculates the FunwithLoop
  */
 
 package funwithloops;
